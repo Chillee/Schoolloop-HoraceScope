1c925218bc87efa916c2853a3d90d34fe2f219a6
Chillee <ben>
2015-08-16 18:02:24 -0700

    added comment box

--- contentscript.js
+++ contentscript.js
@@ -60,12 +60,16 @@ $(document).on('ready',function(){
 		$("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody").append('<tr><td><div class="float_l padding_r5" style="min-width: 105px;"><form><input type="text" name = "category_name" style="width: 100px;" value="Participation"><br><a href="#" title="New Assignment"><input type="text" name = "assignment_name" value = "New Assignment" style="width: 100px;"></form></a></div></td>');
 		$("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr").last().append('<td style="width:100%;"></td>');
 		$("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr").last().append('<td>8/16/15<br></td>');
-		$	("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr").last().append('<td nowrap>\
+		$("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr").last().append('<td nowrap>\
 			 	<div>\
 		            </div>\
 		\
 		\
 		<form><input type="number" name="score" style="width: 25px;" min="0" value=1 text="1"> / <input type="number" name="max_score" style="width: 25px;" min="0" value=1 text="1"></form></td></tr>');
+		$("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr").last().append('<td class="list_text">\
+   		<div style="width: 125px;"></div>\
+		\
+		</td>');
 		$("#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr > td:nth-child(4)").each(function(){
 		//$(this).attr('contentEditable', 'true');
 
@@ -90,7 +94,7 @@ $(document).on('ready',function(){
 		max_score=t[1];
 		console.log(score, max_score);
 		$(this).append('<form><input type="number" name="score" style="width: 25px;" min="1"> / <input type="number" name="max_score" style="width: 25px;" min="1"></form>');
-		
+		//#container_content > div.content_margin > table:nth-child(6) > tbody > tr > td.home_left > table > tbody > tr:nth-child(1) > td:nth-child(4) > form > input[type="number"]:nth-child(2)
 		$(this).find('input[name="score"]').val(parseInt(score));
 		$(this).find('input[name="score"]').text(score);
 
