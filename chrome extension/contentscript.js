51cbc8f3ad98fc29fdbff7cccdd530ced6bc4093
chillee <horacehe2007@yahoo.com>
2015-11-15 21:43:18 -0800

    Updated .crx

--- chrome extension/contentscript.js
+++ chrome extension/contentscript.js
@@ -238,21 +238,21 @@ function initPointObjectCategories(){
 
 function findScores(text){
   if(text == null){
-    return [0, 0];
+    return ["0", "0"];
   }
   var scores = text;
   // console.log(text);
   if(scores.indexOf("Excused") != -1){
-    return [0, 0];
+    return ["0", "0"];
   }
   if(scores.indexOf("/")==-1 && scores.indexOf("-")!=-1){ //Extra Credit
     var extra_points = scores.match(/:.*/g)[0].substring(1);
 
-    return [extra_points, 0];
+    return [extra_points, "0"];
   }
   var grade1 = scores.match(/([0-9]|[" "]|[.])+((\/))/g);
-  if(grade1 == null){
-    return [0, 0];
+  if(grade1 == null || grade1.length == 0){
+    return ["0", "0"];
   }
   grade1 = grade1[0];
   var grade= grade1.substring(0, grade1.length-2);
